5b15cac3ec3ccd10c135cb26b6ead59be9c6bd19
Inuwa Mobarak Abraham <inuwamobarak@gmail.com>
2023-04-16 13:11:58 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import cv2
+from cvzone.FaceDetectionModule import FaceDetector
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+
+detector = FaceDetector(minDetectionCon=0.75)
+
+
+while True:
+    success, img = cap.read()
+    img, bboxes = detector.findFaces(img, draw=True)
+
+    if bboxes:
+        for i, bbox in enumerate(bboxes):
+            x, y, w, h = bbox['bbox']
+            if x < 0: x = 0
+            if y < 0: y = 0
+
+            imgCrop = img[y:y+h, x:x+w]
+            imgBlur = cv2.blur(imgCrop, (35, 35))
+            img[y:y + h, x:x + w] = imgBlur
+            #cv2.imshow(f'Image Cropped {i}', imgCrop)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
